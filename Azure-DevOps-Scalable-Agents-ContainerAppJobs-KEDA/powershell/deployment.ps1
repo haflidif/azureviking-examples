37f2a52ead7cf942a0387cbe4491cafe29a0c31b
Haflidi Fridthjofsson <26624010+haflidif@users.noreply.github.com>
2024-07-13 23:02:50 +0200

    chore: Update deployment script with dynamic resource prefix

--- Azure-DevOps-Scalable-Agents-ContainerAppJobs-KEDA/powershell/deployment.ps1
+++ Azure-DevOps-Scalable-Agents-ContainerAppJobs-KEDA/powershell/deployment.ps1
@@ -9,17 +9,22 @@ $preReqDeployment = @{
     runOnlyPreReq               = $true
 }
 
+
+
 .\bicep-deploy.ps1 @preReqDeployment
 
+$prefix = "ado-keda-demo"
+
 $environmentParameters = @{
-    resourcePrefix              = "cnappjobs-ado-keda"
-    adoResourceGroupName        = "cnappjobs-ado-keda-rg"
-    adoUserAssignedIdentityName = "cnappjobs-ado-keda-mi"
+    resourcePrefix              = $prefix
+    adoResourceGroupName        = "$($prefix)-rg"
+    adoUserAssignedIdentityName = "$($prefix)-mi"
     adoOrganizationName         = "azureviking" # Just the name of the organization, without the https://dev.azure.com/
-    adoPoolName                 = "cnappjobs-ado-keda-pool"
+    adoPoolName                 = "$($prefix)-pool"
     location                    = "westeurope"
     subscriptionId              = "c5c400ba-8a3f-412b-8b13-61832470335a" # Replace with your subscription ID
     tenantId                    = "d0ffafd1-fa67-472b-a574-4dcf8234a4f2" # Replace with your tenant ID
+    runOnlyPreReq               = $true
 }
 
-.\bicep-deploy.ps1 @environmentParameters
+.\bicep-deploy.ps1 @environmentParameters -WhatIf
